221000010555f9cb3c816f4a3822e6fb0aa95903
Alina Garib <alinagrb@gmail.com>
2024-08-30 16:29:49 -0400

    added graph url

--- WikiRace/myapp/models.py
+++ WikiRace/myapp/models.py
@@ -1,21 +1,21 @@
-# from django.db import models
+from django.db import models
 
-# # Create your models here.
+# Create your models here.
 
-# class Vertex(models.Model):
-#     link = models.CharField(max_length=255, unique=True)
+class Vertex(models.Model):
+    link = models.CharField(max_length=255, unique=True)
 
-#     def add_neighbor(self, neighbor):
-#         if not self.neighbors.filter(id=neighbor.id).exists():
-#             self.neighbors.add(neighbor)
+    def add_neighbor(self, neighbor):
+        if not self.neighbors.filter(id=neighbor.id).exists():
+            self.neighbors.add(neighbor)
 
-#     def get_neighbors(self):
-#         return self.neighbors.all()
+    def get_neighbors(self):
+        return self.neighbors.all()
 
-#     # def __str__(self):
-#     #     return self.link
+    # def __str__(self):
+    #     return self.link
 
-# class Edge(models.Model):
-#     from_vertex = models.ForeignKey(Vertex, related_name='outgoing_edges', on_delete=models.CASCADE)
-#     to_vertex = models.ForeignKey(Vertex, related_name='incoming_edges', on_delete=models.CASCADE)
+class Edge(models.Model):
+    from_vertex = models.ForeignKey(Vertex, related_name='outgoing_edges', on_delete=models.CASCADE)
+    to_vertex = models.ForeignKey(Vertex, related_name='incoming_edges', on_delete=models.CASCADE)
 
