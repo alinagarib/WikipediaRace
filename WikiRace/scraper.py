5754aaeda2076908df65ca6b3df9805e8e2329d8
Alina Garib <alinagrb@gmail.com>
2024-08-30 13:06:27 -0400

    Initial commit in WIKIPEDIARACE directory

---
+++ WikiRace/scraper.py
@@ -0,0 +1,85 @@
+import requests
+from graph import Graph
+import time
+from collections import deque
+
+def getWikiLinks(title):
+    S = requests.Session()
+
+    URL = "https://en.wikipedia.org/w/api.php"
+
+    PARAMS = {
+        "action": "query",
+        "format": "json",
+        "titles": f'{title}',
+        "prop": "links",
+        "pllimit": "max"
+    }
+
+    links = []
+
+    while True:
+        response = S.get(url=URL, params=PARAMS)
+        data = response.json()
+        
+        pages = data['query']['pages'] # Response contains dict of pages
+
+        # Gets link titles from page and appends links array
+        for page_id in pages:
+            if 'links' in pages[page_id]:
+                for link in pages[page_id]['links']:
+                    if ':' not in link['title']: # Check to exclued special links
+                        links.append(link['title'])
+
+        # Check for continuation key, allows to check accross multiple pages
+        if 'continue' in data:
+            PARAMS.update(data['continue'])
+        else:
+            break
+
+        time.sleep(0.5)
+
+    return links
+
+def populateGraph(title, g):
+    # Gets all links from a given page
+
+    neighbors = getWikiLinks(title)
+    g.addVertex(title) # Adds vertex into the graph
+
+    for link in neighbors: # Adds all neighbors into the vertex
+        # g.vertices[title].addNeighbor(link)
+        g.addEdge(title, link) # Created edges in the graph from the passed in page to each neighbor
+
+    for link in neighbors:
+        neighbors = getWikiLinks(link)
+        g.addVertex(link)
+        for edge in neighbors:
+            # g.vertices[link].addNeighbor(edge)
+            g.addEdge(link, edge)
+
+def populateBFS(start, g, max=5):
+    queue = deque([(start, 0)])  # Queue to manage BFS, store (link, depth)
+    visited = set()  # Set to track visited pages and avoid re-fetching
+
+    while queue:
+        current_link, depth = queue.popleft()
+
+        if depth > max:
+            continue
+
+        if current_link not in visited:
+            visited.add(current_link)
+            neighbors = getWikiLinks(current_link)
+            print(f"Neighbors of {current_link}: {neighbors}")
+            g.addVertex(current_link)
+
+            for link in neighbors:
+                g.addEdge(current_link, link)
+                if link not in visited:
+                    queue.append((link, depth + 1))
+
+
+if __name__ == "__main__": 
+    links = Graph()
+    populateBFS("Main Page", links)
---
+++ WikiRace/vertex.py
@@ -0,0 +1,35 @@
+# Each page is a vertex
+class Vertex:
+    def __init__(self, link):
+        self.link = link
+        self.adj = [] # list for all outgoing links of a link
+    
+    def addNeighbor(self, neighbor):
+        if neighbor not in self.adj: # checks if link is already a neighbor of a link, if not appends the dict
+            self.adj.append(neighbor)
+
+    def getLink(self):
+        return self.link
+    
+    def getNeighbors(self):
+        return self.adj
+    
+    # def __str__(self):
+    #     result = self.link + " points to "
+
+    #     neighbors_arr = []
+
+    #     # Loop through each neighbor in the neighbors list
+    #     for neighbor in self.adj:
+    #         # Access the link of the neighbor and add it to the neighbor_links list
+    #         neighbors_arr.append(neighbor.link)
+
+    #     # Join the list of neighbor links into a single string, separated by commas
+    #     neighbors_str = ', '.join(neighbors_arr)
+
+    #     # Add the neighbors string to the result
+    #     result += "[" + neighbors_str + "]"
+
+    #     return result
+    
+    
