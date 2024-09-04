cf40e7f8b8063615f5bcda17f43568fa3cec5b9d
Alina Garib <alinagrb@gmail.com>
2024-09-04 13:02:16 -0400

    installed frameworks and added views and urls

--- WikiRace/myapp/urls.py
+++ WikiRace/myapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path("graph/", views.graph, name="graph")
+    path("graph/", views.graph, name="graph"),
+    path('api/graph/', views.get_graph_data, name='graph-data'),
 ]
--- WikiRace/myapp/views.py
+++ WikiRace/myapp/views.py
@@ -3,9 +3,28 @@ from .models import Vertex
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse("hello world")
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from myapp.models import Vertex, Edge
 
-def graph(request):
-    links = Vertex.objects.all()
-    return render(request, "graph.html", {"link": links})
+@api_view(['GET'])
+def get_graph_data(request):
+    vertices = Vertex.objects.all()
+    edges = Edge.objects.all()
+
+    nodes = [{'id': vertex.link} for vertex in vertices]
+    links = [{'source': edge.from_vertex.link, 'target': edge.to_vertex.link} for edge in edges]
+
+    graph_data = {
+        'nodes': nodes,
+        'links': links
+    }
+
+    return Response(graph_data)
+
+# def home(request):
+#     return HttpResponse("hello world")
+
+# def graph(request):
+#     links = Vertex.objects.all()
+#     return render(request, "graph.html", {"link": links})
