13642cc31217f2be590416751a833199cab4d552
Alina Garib <alinagrb@gmail.com>
2024-09-04 16:11:25 -0400

    added shortest path function

--- WikiRace/myapp/algorithms/graph_algorithms.py
+++ WikiRace/myapp/algorithms/graph_algorithms.py
@@ -1,5 +1,6 @@
 import heapq
 import random
+from collections import deque
 from myapp.models import Vertex, Edge
 
 def dijkstra_shortest_path(start_link, end_link):
@@ -54,9 +55,49 @@ def rand_path():
 
         # Check if a path exists between vertex1 and vertex2
         try:
-            dijkstra_shortest_path(vertex1.link, vertex2.link)
+            path_exists(vertex1.link, vertex2.link)
             # If the path is found, return the two vertices
             return vertex1, vertex2
         except Exception:
             # If no path exists, select two new random vertices
             continue
+
+def path_exists(start_link, end_link):
+    try:
+        # Get the start and end vertices from the database
+        start_vertex = Vertex.objects.get(link=start_link)
+        end_vertex = Vertex.objects.get(link=end_link)
+    except Vertex.DoesNotExist:
+        # If one of the vertices doesn't exist, return False
+        return False
+
+    # If the start and end vertices are the same, return True
+    if start_vertex == end_vertex:
+        return True
+
+    # Use a queue for BFS
+    queue = deque([start_vertex])
+    visited = set([start_vertex])  # To track visited vertices
+
+    # Perform BFS to find the path
+    while queue:
+        current_vertex = queue.popleft()
+
+        # Get all outgoing edges from the current vertex
+        edges = Edge.objects.filter(from_vertex=current_vertex)
+
+        for edge in edges:
+            neighbor = edge.to_vertex
+
+            if neighbor == end_vertex:
+                # If we reached the destination vertex, return True
+                return True
+
+            if neighbor not in visited:
+                # Add the neighbor to the queue and mark it as visited
+                queue.append(neighbor)
+                visited.add(neighbor)
+
+    # If BFS completes and we haven't found the end vertex, return False
+    return False
+
